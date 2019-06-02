0011847760e776698977c2d01d33ee91e5aad48d
mark-lally <marquis_mark86@hotmail.com>
2019-06-02 15:44:03 +0100

    Adding Update functionality

--- src/main/java/com/example/marlal/SignUpSheet/controller/SignUpController.java
+++ src/main/java/com/example/marlal/SignUpSheet/controller/SignUpController.java
@@ -3,7 +3,7 @@ package com.example.marlal.SignUpSheet.controller;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,21 +24,30 @@ public class SignUpController {
 	
 	List<Player> thePlayers;
 	
+	// Get a count of all registered players
 	public long getCount() {
+		
 		return playerRepository.count();
 	}
 	
 	@GetMapping("/")
 	public String showPlayers(Model theModel) {
+		
+		// Retrieve all entries from db and count them
 		thePlayers = playerRepository.findAll();
 		long numPlayers = getCount();
+		// Count number of players that selected 'available'
 		long numAvail = playerRepository.countByAvailability(true);
+		
+		// Sort the list based on availability.
+		// Players available will appear at the top.(- changes order direction)
 		Collections.sort(thePlayers, new Comparator<Player>() {
 			@Override
 			public int compare(Player player1, Player player2) {
 				return -Boolean.compare(player1.getAvailability(), player2.getAvailability());
 			}
 		});
+		// Add the results from above to the Model.
 		theModel.addAttribute("players", thePlayers);
 		theModel.addAttribute("playerCount", numPlayers);
 		theModel.addAttribute("numAvail", numAvail);
@@ -49,26 +58,48 @@ public class SignUpController {
 	@GetMapping("/registerForm")
 	public String registerForm(Model theModel) {
 		
-		// Create a blank Player object to bind form data to
+		// Create a 'blank' Player object to bind form data to
 		Player thePlayer = new Player();
 		theModel.addAttribute("player", thePlayer);
 		
 		return "registerForm";
 	}
 	
+	@GetMapping("/info")
+	public String showInfo() {
+		
+		return "info";
+	}
+	
 	@GetMapping("/delete")
 	public String delete(@RequestParam("playerId") int theId) {
-		playerRepository.deleteById(theId);
 		
+		playerRepository.deleteById(theId);
+		// Refresh page to load list based on updated model
 		return "redirect:";
 	}
 	
 	@PostMapping("/save")
 	public String save(@ModelAttribute("player") Player thePlayer) {
 		
-		// save the player
 		playerRepository.save(thePlayer);
 		
 		return "redirect:";
+	}
+	
+	@GetMapping("/update")
+	public String showFormForUpdate(@RequestParam("playerId") int theId, Model theModel) {
+		Player thePlayer;
+		Optional<Player> temp = playerRepository.findById(theId);
+		// temp is either a valid player object or null
+		if(temp.isPresent())
+			thePlayer = temp.get();
+		else // if null
+			throw new RuntimeException("No Player found by the id: "+theId);
+		
+		// Prepopulate the form with the selected players details
+		theModel.addAttribute("player", thePlayer);
+		
+		return "registerForm";
 	}
 }
--- src/main/java/com/example/marlal/SignUpSheet/repository/PlayerRepository.java
+++ src/main/java/com/example/marlal/SignUpSheet/repository/PlayerRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.marlal.SignUpSheet.entity.Player;
 
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
+public interface PlayerRepository extends JpaRepository<Player, Integer> {	
 	
-	
+	/* 
+	 * Uses the Jpa criteria api syntax - query is based off method name.
+	 * For reference: 
+	 * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+	 */ 
 	public long countByAvailability(Boolean trueOrFalse);
 }
